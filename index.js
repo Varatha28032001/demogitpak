0e7491ae1532ec614869387df9a0178fbb9b3b34
varatha.narayan <varatha.narayan@redingtongroup.com>
2024-02-23 17:03:54 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define a simple function to greet the user
+function greet(name) {
+    return `Hello, ${name}!`;
+  }
+  
+  // Export the greet function so it can be used by consumers of the package
+  module.exports = {
+    greet
+  };
+  
